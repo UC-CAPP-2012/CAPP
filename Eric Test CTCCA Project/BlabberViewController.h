7f41358ef6fd8d6c8677c507d898c67bd2f4430a
Hassna <u3050553@uni.canberra.edu.au>
2012-10-04 17:44:28 +1000

    Start of News!!!!!!

--- Eric Test CTCCA Project/BlabberViewController.h
+++ Eric Test CTCCA Project/BlabberViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "News.h"
 
 @interface BlabberViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *newsView;
+@property (strong, nonatomic) IBOutlet UITableView *newsTableView;
+@property (strong, nonatomic) IBOutlet UITableViewCell *newsTableCell;
+@property (strong, nonatomic) IBOutlet UILabel *publishDate;
+@property (strong, nonatomic) IBOutlet UIImageView *newsImage;
+@property (strong, nonatomic) IBOutlet UILabel *newsHeadding;
+@property (strong, nonatomic) IBOutlet UILabel *authorName;
+@property (strong, nonatomic) IBOutlet UILabel *newsContents;
+@property (strong, nonatomic) IBOutlet UIView *newsLoadView;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *newsLoadIndicator;
+
+@property(nonatomic, strong)NSMutableArray *newsList;
+@property (strong, nonatomic)News *currentNews;
+
+@property int NewsID;
+@property (nonatomic, copy) NSData *NewsDateTime;
+@property (nonatomic, copy) NSString *NewsBody;
+@property (nonatomic, copy) NSURL *NewsMediaURL;
+@property (nonatomic, copy) NSString *NewsPublisher;
+@property (nonatomic, copy) NSString *NewsAuthor;
 
 @end
---
+++ Eric Test CTCCA Project/News.h
@@ -0,0 +1,30 @@
+//
+//  News.h
+//  Eric Test CTCCA Project
+//
+//  Created by Hassna Alqarni on 4/10/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface News : NSObject{
+
+    NSString *NewsID;
+    NSString *NewsHeading;
+    NSString *NewsDateTime;
+    NSString *NewsBody;
+    NSURL *NewsMediaURL;
+    NSString *NewsPublisher;
+    NSString *NewsAuthor;
+}
+
+@property (nonatomic, copy) NSString *NewsID;
+@property (nonatomic, copy) NSString *NewsHeading;
+@property (nonatomic, copy) NSString *NewsDateTime; //Should probably be an NSArray of some sort.
+@property (nonatomic, copy) NSString *NewsBody;
+@property (nonatomic, copy) NSURL *NewsMediaURL;
+@property (nonatomic, copy) NSString *NewsPublisher;
+@property (nonatomic, copy) NSString *NewsAuthor;
+
+@end
